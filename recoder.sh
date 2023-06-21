7833513549ac3dd87c2263515cc2d201dfa9295b
Meet Bhanushali <iitjeemeet@gmail.com>
2023-06-21 06:41:24 +0530

    Starting fresh

--- recoder.sh
+++
@@ -1,1 +0,0 @@
-ffmpeg -thread_queue_size 1024 -f alsa -ac 2 -i default -f x11grab -r 30 -s 1920x1080 -i :0.0 -acodec aac -af "highpass=f=200" -vcodec libx264 -preset ultrafast -crf 0 -threads 0 output.mp4
